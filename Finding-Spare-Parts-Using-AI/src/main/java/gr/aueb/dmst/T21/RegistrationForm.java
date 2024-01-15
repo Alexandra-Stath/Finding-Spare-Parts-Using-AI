5e44fe40abe6576a73cfb3d882d46b9929d050fd
ptrWLF2000 <148656620+ptrWLF2000@users.noreply.github.com>
2024-01-15 21:47:07 +0200

    ...

--- Finding-Spare-Parts-Using-AI/src/main/java/gr/aueb/dmst/T21/RegistrationForm.java
+++ Finding-Spare-Parts-Using-AI/src/main/java/gr/aueb/dmst/T21/RegistrationForm.java
@@ -11,105 +11,117 @@ public class RegistrationForm {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Welcome! Choose 1 to Sing in or 2 to Sing up : "); //Σύνδεση ή Εγγραφή
+        System.out.print("Welcome! Choose 1 to Sign in or 2 to Sign up: ");
         String choice = scanner.nextLine();
 
-        if (choice.equals("1")) { //Σύνδεση
-            System.out.println("Sing in");
+        if (choice.equals("1")) {
+            signIn(scanner);
+        } else if (choice.equals("2")) {
+            signUp(scanner);
+        } else {
+            System.out.println("Wrong! Please choose 1 or 2");
+        }
 
-            System.out.print("Username: ");
-            String username = scanner.nextLine();
+        scanner.close();
+    }
 
-            System.out.print("Password: ");
-            String password = scanner.nextLine();
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            } catch(java.lang.ClassNotFoundException e) {
-                System.out.println("ClassNotFoundException: " + e.getMessage());
-                System.exit(0);
-            }
-            try { //Ελεγχος εγκυρότητας στοίχειων χρηστη
-                Connection connection = DriverManager.getConnection("jdbc:mysql://sql8.freesqldatabase.com:3306/sql8676850", "sql8676850", "3QIvIcrqcH");
-    
-                String sql = "SELECT * FROM Users WHERE username = ? AND password = ?";
-    
-                PreparedStatement statement = connection.prepareStatement(sql);
+    private static void signIn(Scanner scanner) {
+        System.out.println("Sign in");
+
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://sql8.freesqldatabase.com:3306/sql8676850", "sql8676850", "3QIvIcrqcH");
+
+            String sql = "SELECT * FROM Users WHERE name = ? AND password = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
                 statement.setString(1, username);
                 statement.setString(2, password);
-    
-                ResultSet resultSet = statement.executeQuery();
-                if (resultSet.next()) { //Κώδικας που τρέχει αφού συμδεθεί ο χρήστης
-                    System.out.println("Sing in completed!");
 
-                    App app = new App();
-                    chatGPT ch = new chatGPT();
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        System.out.println("Sign in completed!");
 
-                    String message = app.model();
+                        App app = new App();
+                        chatGPT ch = new chatGPT();
 
-                    System.out.println(ch.chatGPT(message));
-                } else {
-                    System.out.println("Username or password is wrong.");
+                        String message = app.model();
+                        System.out.println(ch.chatGPT(message));
+                    } else {
+                        System.out.println("Username or password is wrong.");
+                    }
                 }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
             }
-        } else if (choice.equals("2")){ //Εγγραφή
-            System.out.println("Sing up");
+        } catch (ClassNotFoundException | SQLException ex) {
+            handleException(ex);
+        }
+    }
 
-            boolean validInput = false;
-            String username = "";
-            String password = "";
-            String email = "";
+    private static void signUp(Scanner scanner) {
+        System.out.println("Sign up");
 
-            while (!validInput) {
-                System.out.print("New Username: ");
-                username = scanner.nextLine();
+        boolean validInput = false;
+        String username = "";
+        String password = "";
+        String email = "";
 
-                System.out.print("New Password: ");
-                password = scanner.nextLine();
+        while (!validInput) {
+            System.out.print("New Username: ");
+            username = scanner.nextLine();
 
-                System.out.print("E-mail: ");
-                email = scanner.nextLine();
+            System.out.print("New Password: ");
+            password = scanner.nextLine();
 
-                try { //Έλεγχος εγκυρότητας username και email
-                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/T21?useSSL=false", "root", "PrJaT232!84!1");
+            System.out.print("E-mail: ");
+            email = scanner.nextLine();
 
-                    String checkUser = "SELECT * FROM Users WHERE username = ? OR email = ?";
-                    PreparedStatement statement = connection.prepareStatement(checkUser);
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:mysql://sql8.freesqldatabase.com:3306/sql8676850", "sql8676850", "3QIvIcrqcH");
+
+                String checkUser = "SELECT * FROM Users WHERE name = ? OR email = ?";
+                try (PreparedStatement statement = connection.prepareStatement(checkUser)) {
                     statement.setString(1, username);
                     statement.setString(2, email);
-                    ResultSet resultSet = statement.executeQuery();
 
-                    if (resultSet.next()) {
-                        System.out.println("Username or E-mail already exists. Please select something else.");
-                    } else {
-                        validInput = true;
+                    try (ResultSet resultSet = statement.executeQuery()) {
+                        if (resultSet.next()) {
+                            System.out.println("Username or E-mail already exists. Please select something else.");
+                        } else {
+                            validInput = true;
+                        }
                     }
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
                 }
+            } catch (SQLException ex) {
+                handleException(ex);
             }
+        }
 
-            try { //Καταχώρηση στοιχείων χρήστη στη βάση
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/T21?useSSL=false", "root", "PrJaT232!84!1");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://sql8.freesqldatabase.com:3306/sql8676850", "sql8676850", "3QIvIcrqcH");
 
-                String sql = "INSERT INTO Users (username, password, email) VALUES (?, ?, ?)";
-
-                PreparedStatement statement = connection.prepareStatement(sql);
+            String sql = "INSERT INTO Users (name, password, email) VALUES (?, ?, ?)";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
                 statement.setString(1, username);
                 statement.setString(2, password);
                 statement.setString(3, email);
 
                 int rowsInserted = statement.executeUpdate();
                 if (rowsInserted > 0) {
-                    System.out.println("Sing up completed!");
+                    System.out.println("Sign up completed!");
                 }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
             }
-        } else {
-            System.out.println("Wrong! Please choose 1 or 2"); //Μήνυμα λάθος καταχώρησης για εγγραφή ή σύνδεση
+        } catch (SQLException ex) {
+            handleException(ex);
         }
-        scanner.close();
+    }
+
+    private static void handleException(Exception ex) {
+        System.out.println("Exception: " + ex.getMessage());
+        ex.printStackTrace(); // Log or print the full stack trace for debugging purposes
     }
 }
